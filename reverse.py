12e5de621eb17c5f04bbc207449388f6bdc67bce
Khushboo Gupta <kgn9294@gmail.com>
2019-08-28 11:24:25 -0400

    working list of 3
    Max avg reward possible is 3. Needs extra register to work. Also reward is calculated using partial output which starts after the final element is fed to the network.

--- reverse.py
+++ reverse.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     
     verbose = 1
     
-    num_addresses = 4
+    num_addresses = 5
     # register_names = ["rinp","rout","r0"]
-    register_names = ["rinp","rout"]
+    register_names = ["rinp","rout","rtemp"]
     # layer_sizes = {"rinp": 64, "rout":64, "r0": 64, "r1": 64}
     # layer_sizes = {"rinp": 256, "rout": 256, "m": 256}
     layer_sizes = {q: 64 for q in register_names+["m"]}
@@ -54,13 +54,13 @@ if __name__ == "__main__":
   
     # training example generation
     list_symbols = 4
-    min_length = 2
-    max_length = 2
+    min_length = 3
+    max_length = 3
     def training_example():
         list_length = np.random.randint(min_length, max_length+1)
-        inputs = np.array([separator]*(list_length+1))
-        inputs[:-1] = np.random.choice(symbols[1:list_symbols], size=list_length, replace=False)
-        targets = inputs[:-1][::-1]
+        inputs = np.array([separator]*(list_length))
+        inputs[:] = np.random.choice(symbols[1:list_symbols], size=list_length, replace=False)
+        targets = inputs[::-1]
         return inputs, targets
     
     # # reward calculation from LVD
@@ -71,48 +71,64 @@ if __name__ == "__main__":
     #     return -l
     
     # reward calculation based on individual steps
+    # def reward(ghu, targets, outputs):
+    #     idx = [i for i, out in enumerate(outputs) if out != separator]
+    #     outputs_ = [out for out in outputs if out != separator]
+    #     _, d = lvd(outputs_, targets)
+    #     r = np.zeros(len(outputs))
+    #     for i in range(1,d.shape[0]):
+    #         r[idx[i-1]] = +1. if (i < d.shape[1] and d[i,i] == d[i-1,i-1]) else -1.
+
+    #     # # Additional term to penalize time-steps where actions did not match
+    #     # for t in range(1, len(outputs)):
+    #     #     matches = []
+    #     #     for q in ghu.layer_sizes.keys():
+    #     #         matches.append(ghu.ac[t-1][q] == ghu.ac[t][q])
+    #     #     for p in range(len(ghu.plastic)):
+    #     #         matches.append(ghu.pc[t-1][p] == ghu.pc[t][p])
+    #     #     r[t] -= (1. - float(sum(matches)) / len(matches)) / len(outputs)
+
+    #     # Additional term to penalize empty outputs
+    #     if len(outputs_) == 0: 
+    #         r[-1] -= (len(outputs)+1)
+
+    #     return r
+
     def reward(ghu, targets, outputs):
-        idx = [i for i, out in enumerate(outputs) if out != separator]
-        outputs_ = [out for out in outputs if out != separator]
-        _, d = lvd(outputs_, targets)
+        outputs_ = [out for out in outputs[len(targets)-1:] if out!=separator]
+        zeros = [o for o in outputs[len(targets)-1:] if o==separator]
+        totzeros = len(zeros)
         r = np.zeros(len(outputs))
-        for i in range(1,d.shape[0]):
-            r[idx[i-1]] = +1. if (i < d.shape[1] and d[i,i] == d[i-1,i-1]) else -1.
-
-        # # Additional term to penalize time-steps where actions did not match
-        # for t in range(1, len(outputs)):
-        #     matches = []
-        #     for q in ghu.layer_sizes.keys():
-        #         matches.append(ghu.ac[t-1][q] == ghu.ac[t][q])
-        #     for p in range(len(ghu.plastic)):
-        #         matches.append(ghu.pc[t-1][p] == ghu.pc[t][p])
-        #     r[t] -= (1. - float(sum(matches)) / len(matches)) / len(outputs)
-
-        # Additional term to penalize empty outputs
-        if len(outputs_) == 0: r[-1] -= len(targets)
-
+        if len(outputs_)==0:
+            r[-1] -= (len(outputs[len(targets):]))
+        else:
+            _,d = lvd(outputs_,targets) 
+            for i in range(1,d.shape[0]):
+                r[-1] += 1. if (i < d.shape[1] and d[i,i] == d[i-1,i-1]) else -1.
+            r[-1] -= 0.1*totzeros
         return r
     
     # Optimization
     avg_rewards, grad_norms = reinforce(
         ghu_init,
-        num_epochs = 200,
-        num_episodes = 7000,
-        episode_duration = 2*max_length+1,
+        num_epochs = 500,
+        num_episodes = 1000,
+        episode_duration = 2*max_length-1,
         training_example = training_example,
         reward = reward,
         task = "reverse",
-        learning_rate = .25,
+        learning_rate = .2,
         verbose=2)
     
-    # pt.subplot(2,1,1)
-    # pt.plot(avg_rewards)
-    # pt.title("Learning curve")
-    # pt.ylabel("Avg Reward")
-    # pt.subplot(2,1,2)
-    # pt.plot(grad_norms)
-    # pt.xlabel("Epoch")
-    # pt.ylabel("||Grad||")
-    # pt.show()
+    pt.subplot(2,1,1)
+    pt.plot(avg_rewards)
+    pt.title("Learning curve for reverse")
+    pt.ylabel("Avg Reward")
+    pt.subplot(2,1,2)
+    pt.plot(grad_norms)
+    pt.xlabel("Epoch")
+    pt.ylabel("||Grad||")
+    pt.savefig("reverse.png")
+    pt.show()
 
 
